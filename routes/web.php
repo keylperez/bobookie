c5979c611d6546a9ba78acd1b6434fa84091f8bd
keylperez <perez.williamcaleb@gmail.com>
2021-12-10 00:02:56 +0800

    auth added

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('app');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
