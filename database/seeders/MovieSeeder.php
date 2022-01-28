441489d628893db36d3c7f6bd3a201f119931003
marcvaleros <marcnathanielvaleros@gmail.com>
2022-01-28 16:43:11 +0800

    added more movie seed

--- database/seeders/MovieSeeder.php
+++ database/seeders/MovieSeeder.php
@@ -42,6 +42,84 @@ class MovieSeeder extends Seeder
                 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
                 'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ),
+            array(
+                'title' => 'Moon Knight',
+                'price' => 230.00,
+                'rating' => 'PG-13',
+                'year' => 2022,
+                'runtime' => '2hr 12min',
+                'img' => 'movies/moonknight_poster.jpeg',
+                'description' => 'Moon Knight is a fictional character appearing in American comic books published by Marvel Comics. Created by writer Doug Moench and artist Don Perlin, the character first appeared in Werewolf by Night #32.',
+                'start_date' => Carbon::parse('2021-12-8'),
+                'end_date' => Carbon::parse('2022-01-8'),
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ),
+            array(
+                'title' => 'Spiderman: No Way Home',
+                'price' => 300.00,
+                'rating' => 'PG-13',
+                'year' => 2022,
+                'runtime' => '3hr 45min',
+                'img' => 'movies/spiderman_poster.jpg',
+                'description' => "With Spider-Man's identity now revealed, our friendly neighborhood web-slinger is unmasked and no longer able to separate his normal life as Peter Parker from the high stakes of being a superhero. When Peter asks for help from Doctor Strange, the stakes become even more dangerous, forcing him to discover what it truly means to be Spider-Man.",
+                'start_date' => Carbon::parse('2021-12-8'),
+                'end_date' => Carbon::parse('2022-01-8'),
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ),
+            array(
+                'title' => 'Matrix',
+                'price' => 500.00,
+                'rating' => 'PG-13',
+                'year' => 2022,
+                'runtime' => '2hr 45min',
+                'img' => 'movies/matrix_poster.jpg',
+                'description' => "To find out if his reality is a physical or mental construct, Mr. Anderson, aka Neo, will have to choose to follow the white rabbit once more. If he's learned anything, it's that choice, while an illusion, is still the only way out of -- or into -- the Matrix. Neo already knows what he has to do, but what he doesn't yet know is that the Matrix is stronger, more secure and far more dangerous than ever before.",
+                'start_date' => Carbon::parse('2021-12-8'),
+                'end_date' => Carbon::parse('2022-01-8'),
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ),
+            array(
+                'title' => 'Shang-Chi',
+                'price' => 500.00,
+                'rating' => 'PG-13',
+                'year' => 2022,
+                'runtime' => '2hr 45min',
+                'img' => 'movies/shangchi_poster.jpg',
+                'description' => "To find out if his reality is a physical or mental construct, Mr. Anderson, aka Neo, will have to choose to follow the white rabbit once more. If he's learned anything, it's that choice, while an illusion, is still the only way out of -- or into -- the Matrix. Neo already knows what he has to do, but what he doesn't yet know is that the Matrix is stronger, more secure and far more dangerous than ever before.",
+                'start_date' => Carbon::parse('2021-12-8'),
+                'end_date' => Carbon::parse('2022-01-8'),
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ),
+            array(
+                'title' => 'Avatar 2',
+                'price' => 600.00,
+                'rating' => 'PG-13',
+                'year' => 2022,
+                'runtime' => '3hr 45min',
+                'img' => 'movies/avatar_poster.jpg',
+                'description' => "Jake Sully and Ney'tiri have formed a family and are doing everything to stay together. However, they must leave their home and explore the regions of Pandora. When an ancient threat resurfaces, Jake must fight a difficult war against the humans.",
+                'start_date' => Carbon::parse('2021-12-8'),
+                'end_date' => Carbon::parse('2022-01-8'),
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ),
+            array(
+                'title' => 'Encanto',
+                'price' => 299.00,
+                'rating' => 'PG-13',
+                'year' => 2022,
+                'runtime' => '2hr 30min',
+                'img' => 'movies/encanto_poster.jpeg',
+                'description' => "Encanto tells the tale of an extraordinary family, the Madrigals, who live hidden in the mountains of Colombia, in a magical house, in a vibrant town, in a wondrous, charmed place called an Encanto.",
+                'start_date' => Carbon::parse('2021-12-8'),
+                'end_date' => Carbon::parse('2022-01-8'),
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            )
         );
 
         DB::table('movies')->insert($data);
--- database/seeders/MovieTimeslotSeeder.php
+++ database/seeders/MovieTimeslotSeeder.php
@@ -31,7 +31,55 @@ class MovieTimeslotSeeder extends Seeder
             array(
                 'movie_id' => '2',
                 'timeslot_id' => '3',
-            )
+            ),
+            array(
+                'movie_id' => '3',
+                'timeslot_id' => '1',
+            ),
+            array(
+                'movie_id' => '3',
+                'timeslot_id' => '2',
+            ),
+            array(
+                'movie_id' => '4',
+                'timeslot_id' => '1',
+            ),
+            array(
+                'movie_id' => '4',
+                'timeslot_id' => '3',
+            ),
+            array(
+                'movie_id' => '5',
+                'timeslot_id' => '2',
+            ),
+            array(
+                'movie_id' => '5',
+                'timeslot_id' => '3',
+            ),
+            array(
+                'movie_id' => '6',
+                'timeslot_id' => '1',
+            ),
+            array(
+                'movie_id' => '6',
+                'timeslot_id' => '3',
+            ),
+            array(
+                'movie_id' => '7',
+                'timeslot_id' => '2',
+            ),
+            array(
+                'movie_id' => '7',
+                'timeslot_id' => '3',
+            ),
+            array(
+                'movie_id' => '8',
+                'timeslot_id' => '4',
+            ),
+            array(
+                'movie_id' => '8',
+                'timeslot_id' => '3',
+            ),
         );
         DB::table('movie_timeslot')->insert($data);
         //
